c787e6badfef2ff9e147ba788e5031c36bf3010f
DryLeonhard <103483324+DryLeonhard@users.noreply.github.com>
2022-12-16 18:54:42 +0900

    Add files via upload

---
+++ src/main/java/com/codestates/pretutorial/PreTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.pretutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/pretutorial/PreTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.pretutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
